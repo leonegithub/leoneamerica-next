52b5ed0ef92bf9d4c3e42b7acdbee61c683445e2
leonegithub <webmaster@leone.it>
2024-10-24 13:39:45 +0200

    fix: header import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
 import Header from "./Header";
 import Footer from "./Footer";
+import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
   title: "Leone America",
