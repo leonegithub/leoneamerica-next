f7c8ec18f373bdbe5ceb4b81233dec08bd441a4b
leonegithub <webmaster@leone.it>
2025-02-11 13:42:12 +0100

    feat: registration

--- src/app/Eder.tsx
+++ src/app/Eder.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Offcanvas } from "react-bootstrap";
 import AccordionComponent from "@/components/accordion";
 import DropdownComponent from "@/components/dropdown";
