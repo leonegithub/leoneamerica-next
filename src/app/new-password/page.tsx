ee237980c6aba363ecc004dd1828843fa3562779
leonegithub <webmaster@leone.it>
2025-02-11 15:53:03 +0100

    feat: reset password page

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -4,6 +4,7 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
@@ -64,69 +65,71 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className="container">
-      <div className="p-4">
-        <h3 className="mb-4 text-xl font-semibold">Insert a new password</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row md:space-x-4">
-            <div className="relative mb-4 flex-1">
-              <input
-                type="password"
-                id="IN_Pwd"
-                name="IN_Pwd"
-                placeholder="Password"
-                required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  validatePassword(e.target.value, passwordConfirm);
-                }}
-                className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-              <i
-                className="fa-regular fa-eye absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                onClick={() => togglePasswordType("IN_Pwd")}
-              ></i>
+    <Suspense>
+      <div className="container">
+        <div className="p-4">
+          <h3 className="mb-4 text-xl font-semibold">Insert a new password</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col md:flex-row md:space-x-4">
+              <div className="relative mb-4 flex-1">
+                <input
+                  type="password"
+                  id="IN_Pwd"
+                  name="IN_Pwd"
+                  placeholder="Password"
+                  required
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    validatePassword(e.target.value, passwordConfirm);
+                  }}
+                  className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <i
+                  className="fa-regular fa-eye absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => togglePasswordType("IN_Pwd")}
+                ></i>
+              </div>
+              <div className="relative mb-4 flex-1">
+                <input
+                  type="password"
+                  id="IN_Pwd_confirm"
+                  name="IN_Pwd_confirm"
+                  placeholder="Repeat Password"
+                  required
+                  value={passwordConfirm}
+                  onChange={(e) => {
+                    setPasswordConfirm(e.target.value);
+                    validatePassword(password, e.target.value);
+                  }}
+                  className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <i
+                  className="fa-regular fa-eye absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => togglePasswordType("IN_Pwd_confirm")}
+                ></i>
+              </div>
             </div>
-            <div className="relative mb-4 flex-1">
-              <input
-                type="password"
-                id="IN_Pwd_confirm"
-                name="IN_Pwd_confirm"
-                placeholder="Repeat Password"
-                required
-                value={passwordConfirm}
-                onChange={(e) => {
-                  setPasswordConfirm(e.target.value);
-                  validatePassword(password, e.target.value);
-                }}
-                className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-              <i
-                className="fa-regular fa-eye absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                onClick={() => togglePasswordType("IN_Pwd_confirm")}
-              ></i>
+            {errorAlert && (
+              <p id="wrong_pass_alert" className="mb-4 text-red-600">
+                {errorAlert}
+              </p>
+            )}
+            <div className=" mt-2">
+              <button
+                type="submit"
+                id="create"
+                disabled={isSubmitting}
+                className="bg-blue text-white px-6 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:opacity-40"
+              >
+                Update password
+              </button>
             </div>
-          </div>
-          {errorAlert && (
-            <p id="wrong_pass_alert" className="mb-4 text-red-600">
-              {errorAlert}
-            </p>
-          )}
-          <div className=" mt-2">
-            <button
-              type="submit"
-              id="create"
-              disabled={isSubmitting}
-              className="bg-blue text-white px-6 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:opacity-40"
-            >
-              Update password
-            </button>
-          </div>
-        </form>
-        <ToastContainer />
+          </form>
+          <ToastContainer />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
