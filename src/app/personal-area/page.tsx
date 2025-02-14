9ae5e4cb19aec3d44ff88dbfbfa8fc4a510d7562
leonegithub <webmaster@leone.it>
2025-02-14 15:20:23 +0100

    personal area

--- src/app/personal-area/page.tsx
+++ src/app/personal-area/page.tsx
@@ -3,10 +3,16 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
+import UserData from "@/components/PersonalData";
+import "./style.css";
 
 const PersonalArea = () => {
   const router = useRouter();
   const { userId, setUserId } = useAuth();
+  const [data, setData] = useState<User | null>(null);
+  const tabs = ["Profile", "Downloads", "Shop", "Purchased"];
+  const [activeTab, setActiveTab] = useState("Profile");
+
   interface User {
     Nome: string;
     Cognome: string;
@@ -27,8 +33,6 @@ const PersonalArea = () => {
     FlgControllatoPiva: boolean;
     PEC?: string;
   }
-
-  const [data, setData] = useState<User | null>(null);
 
   useEffect(() => {
     if (!userId) {
@@ -60,65 +64,33 @@ const PersonalArea = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container personal flex flex-col justify-between mx-auto p-4">
+      <div className="font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <ul className="flex flex-wrap -mb-px">
+          {tabs.map((tab) => (
+            <li key={tab} className="me-2">
+              <a
+                onClick={() => setActiveTab(tab)}
+                className={`inline-block p-4 border-b-2 rounded-t-lg cursor-pointer ${
+                  activeTab === tab
+                    ? "blue border-blue-600 dark:text-blue-500 dark:border-blue-500"
+                    : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                }`}
+              >
+                {tab}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
       {data ? (
         <>
-          <h1 className="text-3xl font-bold my-4">Your Registration Data</h1>
-          <div className="space-y-2 text-lg leading-relaxed text-gray-800">
-            <p>
-              <strong>Company Name:</strong> {data.RS}
-            </p>
-            <p>
-              <strong>Email:</strong> {data.Email}
-            </p>
-            <p>
-              <strong>First Name:</strong> {data.Nome}
-            </p>
-            <p>
-              <strong>Last Name:</strong> {data.Cognome}
-            </p>
-            <p>
-              <strong>Address:</strong> {data.Indirizzo}
-            </p>
-            <p>
-              <strong>Street Number:</strong> {data.NumeroCivico}
-            </p>
-            <p>
-              <strong>City:</strong> {data["Città"]}
-            </p>
-            <p>
-              <strong>Postal Code:</strong> {data.CAP}
-            </p>
-            <p>
-              <strong>Country:</strong> {data.Paese}
-            </p>
-
-            <p>
-              <strong>Type:</strong> {data.Tipologia}
-            </p>
-            <p>
-              <strong>Consent to Commercial Communications:</strong>{" "}
-              {data.FlgComCom === "0" ? "No" : "Yes"}
-            </p>
-            <p>
-              <strong>Registration Date:</strong> {data.DataInserimento}
-            </p>
-            {data.Paese === "IT" && (
-              <p>
-                <strong>PEC:</strong>{" "}
-                {data.PEC === "" ? (
-                  <span className="text-red-600">Not provided</span>
-                ) : (
-                  data.PEC
-                )}
-              </p>
-            )}
-          </div>
+          {activeTab === "Profile" ? <UserData data={data} /> : ""}
 
           <button
             onClick={handleLogout}
             type="button"
-            className="mt-4 py-2 px-4 bg-blue-700 text-white font-medium rounded hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            className="self-start mt-4 py-2 px-4 bg-blue-700 text-white font-medium rounded hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300"
           >
             Logout
           </button>
