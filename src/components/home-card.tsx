0dba5a886d31546f60bfe66682893f10b9133ac9
leonegithub <webmaster@leone.it>
2025-02-21 11:45:17 +0100

    fix: space-between

--- src/components/home-card.tsx
+++ src/components/home-card.tsx
@@ -13,7 +13,7 @@ export default function HomeCard({ items }: HomeCardProps) {
   return (
     <>
       {items.map((item, index) => (
-        <div key={index} className="product-card ">
+        <div key={index} className="product-card flex flex-col justify-between">
           <div
             className="squared"
             style={{
@@ -29,11 +29,13 @@ export default function HomeCard({ items }: HomeCardProps) {
           /> */}
           <h4>{item.title}</h4>
           <p>{item.text}</p>
-          <button>
-            <Link href={item.link} target="_blank">
-              Learn more
-            </Link>
-          </button>
+          <div className="btn-container">
+            <button className="self-start">
+              <Link href={item.link} target="_blank">
+                Learn more
+              </Link>
+            </button>
+          </div>
         </div>
       ))}
     </>
