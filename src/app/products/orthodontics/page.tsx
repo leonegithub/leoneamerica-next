ceacc2173bee578356b1298eea179e68b6e41254
leonegithub <webmaster@leone.it>
2025-03-06 09:16:20 +0100

    feat: added link for family page

--- src/app/products/orthodontics/page.tsx
+++ src/app/products/orthodontics/page.tsx
@@ -59,6 +59,14 @@ export default function Prodotti() {
             All products
           </button>
         </Link>
+        <Link href={`orthodontics/family-example`}>
+          <button
+            type="button"
+            className="self-start my-5 text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-4 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Example Family Page
+          </button>
+        </Link>
       </div>
     </div>
   );
