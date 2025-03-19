5b7c7ae5c4baab23eb37c549c3175642dbb8cccd
leonegithub <webmaster@leone.it>
2025-03-19 09:46:23 +0100

    fix: set selected image

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -27,15 +27,13 @@ export default function Gallery({ featured, images }: GalleryProps) {
       </div>
       <div className="grid grid-cols-8 gap-4">
         {validImages.map((image, index) => (
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() => setSelectedImage(image)}
-            key={index}
-          >
+          <div onClick={() => setSelectedImage(image)} key={index}>
             <Image
               height={500}
               width={500}
-              className="h-auto p-2 max-w-full rounded-lg"
+              className={`h-auto product-image p-2 max-w-full rounded-lg ${
+                selectedImage === image ? "selected" : ""
+              }`}
               src={image ?? null}
               alt={`img-element ${index}`}
             />
